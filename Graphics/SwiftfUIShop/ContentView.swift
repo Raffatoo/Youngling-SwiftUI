d0bafbf75a33e061a558c8c4054f40e97a8a8317
Rafael Torres <39065424+Raffatoo@users.noreply.github.com>
2021-06-13 06:45:03 -0500

    Star SwiftUIShop
    SwiftUI Graphic

---
+++ Graphics/SwiftfUIShop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftfUIShop
+//
+//  Created by Cruz Torres on 13/06/21.
+//  Copyright © 2021 Cruz Torres. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
